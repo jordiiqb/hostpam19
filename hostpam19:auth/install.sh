3d7559d308f7833e1af825f82a0026138e427564
jordi_quiros <jordiiqb@gmail.com>
2019-11-18 09:41:54 +0100

    Creació del container hostpam19:auth per poder fer autenticació amb ldap.

--- hostpam19:auth/install.sh
+++ hostpam19:auth/install.sh
@@ -8,5 +8,5 @@ echo "local3" | passwd --stdin local3
 cp /opt/hostpam/login.defs /etc/login.defs
 cp /opt/hostpam/system-auth /etc/pam.d/system-auth
 cp /opt/hostpam/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
-
-
+cp /opt/hostpam/nslcd.conf /etc/nslcd.conf
+cp /opt/hostpam/nsswitch.conf /etc/nsswitch.conf
